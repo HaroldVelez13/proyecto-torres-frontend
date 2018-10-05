9fd11786d5ce46215893a54378cfbdb5e654f848
HaroldVelez13 <haroldvelez113@gmail.com>
2018-10-05 08:06:19 -0500

    Add grid Eps

--- src/app/eps/eps.module.ts
+++ src/app/eps/eps.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import {FlexLayoutModule} from '@angular/flex-layout';
-
-import { 
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';import { 
   MatButtonModule, 
   MatIconModule,  
   MatFormFieldModule,
@@ -29,7 +26,7 @@ const epesRoutes: Routes = [
     RouterModule.forChild(epesRoutes),
     FormsModule,
     ReactiveFormsModule,
-    FlexLayoutModule,
+    
 	  MatButtonModule, 
   	MatIconModule,  
   	MatFormFieldModule,
