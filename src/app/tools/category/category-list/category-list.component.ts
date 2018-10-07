1e6b611a603cd5af908bd8d664902c2d3fa940bf
HaroldVelez13 <haroldvelez113@gmail.com>
2018-10-07 01:00:44 -0500

    add responsibe slider

--- src/app/tools/category/category-list/category-list.component.ts
+++ src/app/tools/category/category-list/category-list.component.ts
@@ -24,6 +24,26 @@ export class CategoryListComponent implements OnInit {
   public slideConfig = {    'centerMode': true,
                             'slidesToShow': 3,
                             'dots': true,
+                            responsive: [
+                              {
+                                'breakpoint': 768,
+                                'settings': {
+                                  'arrows': false,
+                                  'centerMode': true,
+                                  'centerPadding': '40px',
+                                  'slidesToShow': 2
+                                }
+                              },
+                              {
+                                'breakpoint': 480,
+                                'settings': {
+                                  'arrows': false,
+                                  'centerMode': true,
+                                  'centerPadding': '40px',
+                                  'slidesToShow': 1
+                                }
+                              }
+                            ]
                                                 
                         };
 
